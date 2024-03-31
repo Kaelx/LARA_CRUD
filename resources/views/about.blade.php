b0e5deb8de7bdc26135f1aa5aad454060751874c
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-03-31 21:27:37 +0800

    Update about page content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
 @section('content')
     <h1>About</h1>
     <p>CRUD operations are the foundation of any data-driven application. On this page, we've implemented these operations in a user-friendly manner, allowing you to manage data efficiently and effectively. You can easily create new records, retrieve existing records, update records, and delete records as needed.</p>
-    <p>We hope you find our application easy to use and beneficial for your data management needs. If you have any questions or need further assistance, please don't hesitate to contact us.</p>
+    <p>We hope you find this website easy to use. If you have any questions or need further assistance, please don't hesitate to contact us.</p>
 @endsection

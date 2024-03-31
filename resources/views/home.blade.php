23fb08b35f8e5e9e46562dcfb7110cfd1659dcd8
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-03-31 17:27:06 +0800

    more update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('components.layout')
 @section('title', 'Home')
 
 @section('content')
@@ -17,19 +17,20 @@
     </div>
 
     <div class="card-body">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Contact</th>
-                    <th>Time Created</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($students as $student)
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Time Created</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($students as $student)
                     <tr>
                         <td>{{ $student->id }}</td>
                         <td>{{ $student->name }}</td>
@@ -37,20 +38,42 @@
                         <td>{{ $student->contact }}</td>
                         <td>{{ $student->created_at }}</td>
                         <td>
-                            <a href="{{ route('students.edit', ['student' => $student]) }}" class="btn btn-secondary">EDIT</a>
+                            <a href="{{ route('students.edit', ['student' => $student]) }}" class="btn btn-secondary mb-2">EDIT</a>
+                            <button type="button" class="btn btn-danger mb-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Delete</button>
 
-                            <form action="{{route('students.delete',['student'=> $student] )}}" method="POST" class="d-inline"> 
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">DELETE</button>
-                            </form>
+
+                            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">CONFIRM</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Are you sure you want to delete this data?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                            <form action="{{ route('students.delete', ['student' => $student]) }}" method="POST"
+                                                class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
-                @endforeach
-            </tbody>
-
-        </table>
-
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+    
+
 
 @endsection

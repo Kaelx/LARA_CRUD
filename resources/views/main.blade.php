68d1175d144df77eb38cf539c9dbe0a191cf86b9
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-04-06 21:59:48 +0800

    small changes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -41,16 +41,14 @@
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel{{ $student->id }}">
-                                                        CONFIRM</h1>
+                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel{{ $student->id }}">CONFIRM</h1>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
                                                     Are you sure you want to delete this data?
                                                 </div>
                                                 <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Cancel</button>
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                                                     <form action="{{ route('delete', ['student' => $student]) }}" method="POST" class="d-inline">
                                                         @csrf
                                                         @method('DELETE')
